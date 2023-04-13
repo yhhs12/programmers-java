ee212653d1af5c577ef8f053853118f1cef34edb
yhhs12 <yhhs12@naver.com>
2023-04-13 20:44:47 +0900

    feat(programmers):commit

--- 0403/Evenandodd.java
+++ 0403/Evenandodd.java
@@ -1,5 +1,6 @@
 class Evenandodd {
     public String solution(int num) {        
-        return num % 2 == 0? "Even" : "Odd";  //정수n을 2로 나눈 나머지가 0이면 짝수 아니면 홀수;
+        return num % 2 == 0? "Even" : "Odd";  //정수n을 2로 나눈 나머지가 0이면 짝수 아니면 홀수
+        int b = 0;
     }
 }
--- 0403/the sum of the mineral water.java
+++ 0403/the sum of the mineral water.java
@@ -3,6 +3,7 @@ class thesumofthemineralwater {
         int answer = 0;
         for(int i = 1; i <= n; i++){  //i가 1부터 n까지 증가할 때
             if(n % i == 0){  //n을 i로 나누었을 때 0이면
+                int a = 0;
                 answer += i;  //answer에 i를 더한다.
             }
         }
